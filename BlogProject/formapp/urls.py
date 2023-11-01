ce5e200a3c38b5046cf8798b52026ccc475f1f02
Kamran Hussain <kamranh@amdocs.com>
2023-11-01 14:47:19 +0530

    added class based view FormView, ListView, CreateView, DetailView

--- BlogProject/formapp/urls.py
+++ BlogProject/formapp/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from formapp import views
-from formapp.views import reviewes, ThankyouView
+from formapp.views import reviewes, ThankyouView,ReviewsListView, SingleReviewView
 urlpatterns = [
     path('',reviewes.as_view(),name='reviews'),
     path('thank-you',ThankyouView.as_view(),name='thanks'),
+    path('reviews_list',ReviewsListView.as_view(),name='review-list'),
+    path('single_review/<int:pk>',SingleReviewView.as_view(),name='singlereview'),
+    #path('single_review/<int:id>',SingleReviewView.as_view(),name='singlereview'),
     #path('',views.reviews,name='reviews'), for function based views
     #path('thank-you',views.thank_you,name="thanks"), function based views
 ]
--- BlogProject/formapp/views.py
+++ BlogProject/formapp/views.py
@@ -1,8 +1,14 @@
+from typing import Any
+from django.db import models
 from django.shortcuts import render
-from django.http import HttpResponseRedirect
+from django.http import HttpResponse, HttpResponseRedirect
 from formapp.forms import ReviewForm
 from formapp.models import ReviewApp
 from django.views import View
+# this is for static templates but we can also pass dynamic content via context
+from django.views.generic.base import TemplateView 
+from django.views.generic.edit import FormView, CreateView # it will only create form and does not save in db automatically
+from django.views.generic import ListView, DetailView
 # Create your views here.
 
 
@@ -49,18 +55,71 @@ from django.views import View
 
 #using class based view
 #ReviewView convention
-class reviewes(View):
-    def get(self,request):
-        form = ReviewForm()
-        return render(request,'formapp/home.html',{'form':form})
-    def post(self,request):
-        form = ReviewForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return HttpResponseRedirect('/formapp/thank-you')
+# class reviewes(View):
+#     def get(self,request):
+#         form = ReviewForm()
+#         return render(request,'formapp/home.html',{'form':form})
+#     def post(self,request):
+#         form = ReviewForm(request.POST)
+#         if form.is_valid():
+#             form.save()
+#             return HttpResponseRedirect('/formapp/thank-you')
+
+# this will save data using save method but we can also improve this
+# class reviewes(FormView):
+#     template_name = 'formapp/home.html'
+#     form_class = ReviewForm
+#     success_url = '/formapp/thank-you'
+
+#     def form_valid(self, form):
+#         form.save()
+#         return super().form_valid(form)
         
 
+class reviewes(CreateView):
+    template_name = 'formapp/home.html'
+    model = ReviewApp
+    fields = '__all__'
+    success_url = '/formapp/thank-you'
 
-class ThankyouView(View):
-    def get(self,request):
-        return render(request,'formapp/thank-you.html')
+
+# class ThankyouView(View):
+#     def get(self,request):
+#         return render(request,'formapp/thank-you.html')
+
+
+class ThankyouView(TemplateView):
+    template_name = 'formapp/thank-you.html'
+    def get_context_data(self, **kwargs: Any):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'Return dynamic data using template view get_context_data',
+        return context
+    
+
+# class ReviewsListView(TemplateView):
+#     template_name = 'formapp/reviews_list.html'
+#     def get_context_data(self, **kwargs: Any):
+#         context = super().get_context_data(**kwargs)
+#         context['Reviews_List'] = ReviewApp.objects.all()
+#         return context
+
+class ReviewsListView(ListView):
+    model = ReviewApp
+    template_name = 'formapp/reviews_list.html'
+    context_object_name = 'Reviews_List'
+    
+
+# class SingleReviwView(TemplateView):
+#     template_name = 'formapp/review_detail.html'
+#     def get_context_data(self, **kwargs: Any):
+#         context = super().get_context_data(**kwargs)
+#         review_id =  kwargs['id']
+#         single_review = ReviewApp.objects.get(pk=review_id)
+#         context['single'] = single_review
+#         return context
+    
+
+class SingleReviewView(DetailView):
+    model = ReviewApp
+    template_name = 'formapp/review_detail.html'
+    context_object_name = 'single'
